a34ed541c559971b8834dd8593fd25148437f38a
callumok2004 <callumokane123@gmail.com>
2021-06-21 04:03:29 +0100

    🌟 Code Cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet(),
       originalRenderPage = ctx.renderPage
 
