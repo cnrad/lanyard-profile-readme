2c12055a5fb4ea392058fb54bf8af690cc35a162
callumok2004 <callumokane123@gmail.com>
2021-06-21 04:02:15 +0100

    🌟 Code Cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,6 @@ export default class MyDocument extends Document {
         })
 
       const initialProps = await Document.getInitialProps(ctx);
-
       return {
         ...initialProps,
         styles: (
