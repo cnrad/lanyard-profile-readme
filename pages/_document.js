7a1c636cf8e1a1c4340c465f72dcbc27cc697e88
callumok2004 <callumokane123@gmail.com>
2021-06-21 04:01:23 +0100

    🌟 Code Cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -9,11 +9,11 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
         })
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
+
       return {
         ...initialProps,
         styles: (
