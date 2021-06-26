e4bad90eb20b10433203709b85c805c9d72df588
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-26 12:21:59 -0400

    Refactoring: _document is not a ts file

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: any) {
+        const sheet = new ServerStyleSheet(),
+            originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+}
