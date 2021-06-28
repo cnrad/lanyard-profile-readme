d943812de7777eedcf50fd444d8870df72642b8b
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-06-27 21:18:37 -0400

    remove useless break statement
    Co-Authored-By: looskie <50378828+Looskie@users.noreply.github.com>

--- src/renderCard.tsx
+++ src/renderCard.tsx
@@ -52,7 +52,6 @@ const renderCard = async (body: LanyardTypes.Root, params: Parameters): Promise<
                 break;
             default:
                 avatar = blue;
-                break
         }
     }
 
