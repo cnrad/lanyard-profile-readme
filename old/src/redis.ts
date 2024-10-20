15c97548ffbe26da6b0f6a87170744809d12229f
Hexagonn <128217934+hexaaagon@users.noreply.github.com>
2024-10-20 13:20:43 +0000

    delete: old next.js file

--- old/src/redis.ts
+++
@@ -1,9 +0,0 @@
-import Redis from "ioredis";
-
-const redis = new Redis(process.env.REDIS_URL, {
-    connectTimeout: 1000,
-    lazyConnect: false,
-    maxRetriesPerRequest: 1,
-});
-
-export default redis;
