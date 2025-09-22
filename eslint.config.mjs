dd841ce04d311a4c192e4adb11f2a89c10872245
Alexander Hamilton <a.hamilton72006@gmail.com>
2025-09-21 17:29:12 -0700

    fix: disable no-img-element rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
