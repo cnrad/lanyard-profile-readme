6c2ee1f2f2b3b2835dcfb30a4ad551337a4e2854
Alexander Hamilton <a.hamilton72006@gmail.com>
2025-09-22 23:13:25 -0700

    transition lint silencer to be per-file. It also turns out er don't need <img> in page.tsx
    I guess that's why the warning should be per-file lol

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@next/next/no-img-element": "off",
-    },
-  },
 ];
 
 export default eslintConfig;
